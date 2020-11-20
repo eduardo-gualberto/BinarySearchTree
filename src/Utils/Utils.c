920785fd2facda5b47fcc0a7a3dbda1e31a2af48
eduardo-gualberto <eduardo.santos.gualberto@gmail.com>
2020-11-20 16:49:18 -0300

    feita parte da inserção do balanceamento da arvore

--- src/Utils/Utils.c
+++ src/Utils/Utils.c
@@ -24,23 +24,101 @@ Tree_t *__pushR(Tree_t *bst, void *key, void *value, int *hchange)
     Tree_t *child;
     if (bst->compare(bst->key, key) == 1)
     {
-      child = push(bst->left, key, value);
+      child = __pushR(bst->left, key, value, hchange);
       child->compare = bst->compare;
       bst->left = child;
+      if (*hchange)
+      {
+        if (bst->balance == +1)
+        {
+          bst->balance = 0;
+          *hchange = 0;
+        }
+        else if (bst->balance == 0)
+        {
+          bst->balance = -1;
+          *hchange = 1;
+        }
+        else if (bst->balance == -1)
+        {
+          if (bst->left->balance == -1)
+          {
+            bst = rotate(bst, RIGHT);
+            bst->right->balance = 0;
+          }
+          else
+          {
+            bst->left = rotate(bst->left, LEFT);
+            bst = rotate(bst, RIGHT);
+            if (bst->balance == 0)
+            {
+              bst->right->balance = bst->left->balance = 0;
+            }
+            else if (bst->balance == -1)
+            {
+              bst->left->balance = 0;
+              bst->right->balance = +1;
+            }
+            else
+            {
+              bst->left->balance = -1;
+              bst->right->balance = 0;
+            }
+          }
+          bst->balance = 0;
+          *hchange = 0;
+        }
+      }
     }
     else
     {
-      child = push(bst->right, key, value);
+      child = __pushR(bst->right, key, value, hchange);
       child->compare = bst->compare;
       bst->right = child;
+      if (*hchange)
+      {
+        if (bst->balance == -1)
+        {
+          bst->balance = 0;
+          *hchange = 0;
+        }
+        else if (bst->balance == 0)
+        {
+          bst->balance = +1;
+          *hchange = 1;
+        }
+        else if (bst->balance == +1)
+        {
+          if (bst->right->balance == +1)
+          {
+            bst = rotate(bst, LEFT);
+            bst->left->balance = 0;
+          }
+          else
+          {
+            bst->right = rotate(bst->right, RIGHT);
+            bst = rotate(bst, LEFT);
+
+            if (bst->balance == 0)
+            {
+              bst->right->balance = bst->left->balance = 0;
+            }
+            else if (bst->balance == +1)
+            {
+              bst->left->balance = 0;
+              bst->right->balance = -1;
+            }
+            else
+            {
+              bst->left->balance = +1;
+              bst->right->balance = 0;
+            }
+          }
+          bst->balance = 0;
+          *hchange = 0;
+        }
+      }
     }
-
-    int rheight = bst->right ? bst->right->height : 0;
-    int lheight = bst->left ? bst->left->height : 0;
-
-    bst->height = (rheight > lheight ? rheight : lheight) + 1;
-
-    bst->balance = rheight - lheight;
 
     child->parent = bst;
     return bst;
